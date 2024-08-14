23028fbb53726196158a9d9887c4eba7a687a9dc
Rohitvgupta15 <143114985+Rohitvgupta15@users.noreply.github.com>
2024-08-14 15:50:20 +0530

    Update top_module.v

--- Top module/top_module.v
+++ Top module/top_module.v
@@ -1,29 +1,33 @@
 module Top_Module(
-    input [7:0] data,
-    input clk,
-    input transmit,
-    input reset,
-    //input clk_fpga,
-    input reset_r,
-    input RxD,
-    output [7:0]RxData,
-    output TxD,
-    output TxD_debug,
-    output transmit_debug,
-    output btn_debug,
-    output clk_debug
-    );
-    
-    wire transmit_out;
-    
-     assign Tx_debug = TxD;
-       assign transmit_debug = transmit_out;
-       assign btn_debug = reset;
-       assign clk_debug = clk;
-       
-    
+    input [7:0] data,        // Input data (8 bits)
+    input clk,               // Clock signal
+    input transmit,          // Transmit signal
+    input reset,             // Reset signal
+    input reset_r,           // Reset signal for receiver
+    input RxD,               // Received data
+    output [7:0] RxData,     // Output received data (8 bits)
+    output TxD,              // Output transmitted data
+    output TxD_debug,        // Debug transmitted data
+    output transmit_debug,   // Debug transmit signal
+    output btn_debug,        // Debug reset signal
+    output clk_debug         // Debug clock signal
+);
+
+    wire transmit_out;       // Wire for debounced transmit signal
+
+    // Assign debug signals
+    assign Tx_debug = TxD;
+    assign transmit_debug = transmit_out;
+    assign btn_debug = reset;
+    assign clk_debug = clk;
+
+    // Transmitter module
     Transmitter T1(clk, reset, transmit, data, TxD);
+
+    // Debounce module
     Debounce_Signals DB(clk, reset, transmit_out);
-    
-     Receiver_RxD uut(.clk_fpga(clk) , .reset(reset_r) , .RxD(RxD) , .RxData(RxData));  
+
+    // Receiver module
+    Receiver_RxD uut(.clk_fpga(clk), .reset(reset_r), .RxD(RxD), .RxData(RxData));  
+
 endmodule
