dbfac79dc57ea3061aec0baedec8ee9421fae78c
Rohitvgupta15 <143114985+Rohitvgupta15@users.noreply.github.com>
2024-08-14 15:52:21 +0530

    Update Debounce Signals.v

--- Debounce Signals.v
+++ Debounce Signals.v
@@ -1,40 +1,40 @@
-module Debounce_Signals #(parameter threshold =2000000)
+module Debounce_Signals #(parameter threshold = 2000000)
 (
-    input clk,
-    input btn, //take button value as input
-    output reg transmit 
-    );
-    
-    reg button_ff1=0; // btn value is fed to ff1
-    reg button_ff2=0; //ff1 is fed to ff2 with 1 cycle delay
-    reg [30:0] count=0; //20 bit count i.e 1000000 (threshold) for increment and decerement when button is pressed or released.
-    
+    input clk,         // Clock signal
+    input btn,         // Button input
+    output reg transmit // Debounced output
+);
 
-    
+    reg button_ff1 = 0; // First flip-flop
+    reg button_ff2 = 0; // Second flip-flop
+    reg [30:0] count = 0; // Counter
+
+    // Capture button state in flip-flops
     always @(posedge clk)
     begin
-        button_ff1<=btn;
-        button_ff2<=button_ff1;
+        button_ff1 <= btn;
+        button_ff2 <= button_ff1;
     end
-    
 
-   
-   always @(posedge clk)
-   begin
-       if (button_ff2 == 1) 
-          begin
-              if (~&count == 1)  // every count is AND and then NOT eg 101 --> 1&0&1 = 0 --> ~0 == 1 if 111 --> 1&1&1 = 1 --> ~1 == 0 it voilate the condition ,also indicate that max limit(1111..) is not reached
-                  count<=count+1; //when btn is pressed, count up
-          end
-       else 
-          begin
-             if (|count == 1) // every count is OR eg 101 --> 1 | 0 | 1 = 1 the condition is satisfy and also indicate that count is not at 0000...  
-                 count<=count-1; //when btn is released, count down 
-          end
-             if (count>threshold) //if the count is larger than the threshhold
-                 transmit<=1; //debounced signal is 1
-             else
-                 transmit<=0; //debounced signal is 0
-  end
+    // Debouncing logic
+    always @(posedge clk)
+    begin
+        if (button_ff2 == 1) 
+        begin
+            if (~&count == 1)  // Count up if button is pressed
+                count <= count + 1;
+        end
+        else 
+        begin
+            if (|count == 1) // Count down if button is released
+                count <= count - 1;
+        end
+
+        // Set output based on count
+        if (count > threshold) 
+            transmit <= 1;
+        else
+            transmit <= 0;
+    end
   
- endmodule
+endmodule
